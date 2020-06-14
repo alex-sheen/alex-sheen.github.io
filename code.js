87eeb274b4672acf2f9234882d6294734fc23536
Alex Sheen <alex.m.sheen@gmail.com>
2020-06-14 13:35:41 -0400

    added cache

--- code.js
+++ code.js
@@ -1,3 +1,15 @@
+function backgammon() {
+    window.location.href = "https://github.com/alex-sheen/backgammon";
+}
+
+function cache() {
+    window.location.href = "https://github.com/alex-sheen/cache-sim";
+}
+
 function fishtank() {
     window.location.href = "code/fishtank.html";
+}
+
+function shell() {
+    window.location.href = "https://github.com/alex-sheen/unix-shell";
 }
