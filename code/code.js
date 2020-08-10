2ac0a1168651ce9a8bac83584d3acca24fab737f
Alex Sheen <alex.m.sheen@gmail.com>
2020-08-09 22:02:09 -0400

    added code.html redirect for old links

--- code/code.js
+++ code/code.js
@@ -10,10 +10,18 @@ function fishtank() {
     window.location.href = "fishProject/fishtank.html";
 }
 
+function juliaset() {
+    window.location.href = "juliaset/index.html";
+}
+
 function shell() {
     window.location.href = "https://github.com/alex-sheen/unix-shell";
 }
 
 function tree() {
     window.location.href = "tree/tree.html";
+}
+
+function tron() {
+    window.location.href = "tron/index.html";
 }
