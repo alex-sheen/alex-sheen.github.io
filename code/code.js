7fe0caef83a77900366a55e439d7529f7ca981e7
Alex Sheen <alex.m.sheen@gmail.com>
2020-09-11 15:05:53 -0400

    added ceres

--- code/code.js
+++ code/code.js
@@ -6,6 +6,13 @@ function cache() {
     window.location.href = "https://github.com/alex-sheen/cache-sim";
 }
 
+function ceres() {
+    window.location.href = "ceres/index.html";
+}
+function ceres_polsky() {
+    window.location.href = "https://polsky.uchicago.edu/2020/05/20/2020-snvc-finalists-announced/";
+}
+
 function chiventure() {
     window.location.href = "chiventure/index.html";//"https://github.com/uchicago-cs/chiventure";
 }
