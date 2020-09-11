d2201ac14f8d33c58b51d8b39d7812ebdd644cfb
Alex Sheen <alex.m.sheen@gmail.com>
2020-09-11 14:39:09 -0400

    added controls for video

--- video/video.js
+++ video/video.js
@@ -1,7 +1,18 @@
+function datamosh() {
+    window.location.href = "datamosh.html";
+}
+
 function spook_world_1() {
     window.location.href = "spook-world-1.html";
 }
 
-function datamosh() {
-    window.location.href = "datamosh.html";
+function spook_youtube() {
+    window.location.href = "https://www.youtube.com/watch?v=r2pNrH2odog";
+}
+
+function toggleSound(img)
+{
+   img.src= img.src=="/assets/images/unmuted.png" ? "/assets/images/muted.png" : "/assets/images/unmuted.png";
+   var video=document.getElementById("vid");
+   video.muted = !video.muted;
 }
