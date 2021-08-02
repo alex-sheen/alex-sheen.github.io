120d0ede7e6c158b4e4a7128f11906832ab35b15
Alex <alexsheen@uchicago.edu>
2021-08-02 13:44:39 -0400

    rabbithole

--- video/video.js
+++ video/video.js
@@ -14,6 +14,10 @@ function d3() {
     window.location.href = "d3.html";
 }
 
+function rabbithole() {
+    window.location.href = "rabbithole.html";
+}
+
 function reel() {
     window.location.href = "reel.html";
 }
